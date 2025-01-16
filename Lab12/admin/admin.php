abb0c6a5df5cbcd9c7649a1cc801341c6a47f505
ArtemTsymbalov <115550345+ArtemTsymbalov@users.noreply.github.com>
2025-01-16 17:51:11 +0100

    Update admin.php
    add ../css/ in line 45

--- Lab12/admin/admin.php
+++ Lab12/admin/admin.php
@@ -42,7 +42,7 @@ if (!isset($_SESSION['zalogowany'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
-    <link rel="stylesheet" href="css/admin-style.css">
+    <link rel="stylesheet" href="../css/admin-style.css">
 </head>
 <body>
 <?php
